bb9dd7761941cb4200c4bcf905f59db3ed8af023
thebakercat <thebakercat542@gmail.com>
2024-06-16 04:08:08 +0500

    dark theme, code cleanup

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,14 +1,26 @@
 import { FC, useContext } from 'react'
 // @ts-ignore
 import RosatomLogo from '@assets/rosatom_logo.svg?react'
-import styles from '@components/login-header/login-header.module.pcss'
-import { Button, Title } from '@mantine/core'
+import {
+  ActionIcon,
+  Button,
+  Title,
+  useComputedColorScheme,
+  useMantineColorScheme
+} from '@mantine/core'
 import { useNavigate } from 'react-router-dom'
 import authContext from '../../contexts/auth-context.tsx'
+import { IconMoon, IconSun } from '@tabler/icons-react'
+import cx from 'clsx'
+import styles from './app-header.module.pcss'
 
 export const AppHeader: FC = () => {
   const navigate = useNavigate()
   const { logout } = useContext(authContext)
+  const { setColorScheme } = useMantineColorScheme({ keepTransitions: true })
+  const computedColorScheme = useComputedColorScheme('light', {
+    getInitialValueInEffect: true
+  })
 
   return (
     <div className={styles.header}>
@@ -19,13 +31,29 @@ export const AppHeader: FC = () => {
             Чат
           </Title>
         </div>
-        <div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <ActionIcon
+            onClick={() =>
+              setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')
+            }
+            variant="default"
+            size="md"
+            aria-label="Toggle color scheme"
+          >
+            <IconSun className={cx(styles.icon, styles.light)} stroke={1.5} />
+            <IconMoon className={cx(styles.icon, styles.dark)} stroke={1.5} />
+          </ActionIcon>
           <Button
             onClick={() => {
               logout()
               navigate('/login')
             }}
-            variant="white"
+            variant="transparent"
             color="red"
           >
             Выйти
